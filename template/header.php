abf539d6696406097556b8b78e45f9aef15e503d
irvanriz <81153415+irvanriz@users.noreply.github.com>
2021-06-23 10:06:49 +0700

    Create header.php

---
+++ template/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Responsive Admin Dashboard Template">
+        <meta name="keywords" content="admin,dashboard">
+        <meta name="author" content="stacks">
+        <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        
+        <!-- Title -->
+        <title>Management Project</title>
+
+        <!-- Styles -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700,800&display=swap" rel="stylesheet">
+        <link href="template/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="template/assets/plugins/font-awesome/css/all.min.css" rel="stylesheet">
+        <link href="template/assets/plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
+        <link href="template/assets/plugins/apexcharts/apexcharts.css" rel="stylesheet">
+        <link href="template/assets/plugins/DataTables/datatables.min.css" rel="stylesheet"> 
+
+      
+        <!-- Theme Styles -->
+        <link href="template/assets/css/main.min.css" rel="stylesheet">
+        <link href="template/assets/css/custom.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+      <div class='loader'>
+        <div class='spinner-grow text-primary' role='status'>
+          <span class='sr-only'>Loading...</span>
+        </div>
+      </div>
+        <div class="page-container">
+            <div class="page-header">
+                <nav class="navbar navbar-expand-lg d-flex justify-content-between">
+                  <div class="" id="navbarNav">
+                    <ul class="navbar-nav" id="leftNav">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="" id="navbarNav">
+                    <ul class="navbar-nav" id="leftNav">
+                      <li class="nav-item">
+                        <p style="font-size: 25px;font-weight: bold;margin-left: -170px;text-transform: uppercase;color: #7888fc;">Aplikasi Manajemen Project</p>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+            </div>
