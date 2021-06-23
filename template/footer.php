1ab8258029e0618dccb9eec72138b8ff03b59a58
irvanriz <81153415+irvanriz@users.noreply.github.com>
2021-06-23 10:11:47 +0700

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,17 @@
+              
+            </div>
+        </div>
+        
+        <!-- Javascripts -->
+        <script src="template/assets/plugins/jquery/jquery-3.4.1.min.js"></script>
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
+        <script src="template/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/feather-icons"></script>
+        <script src="template/assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
+        <script src="template/assets/plugins/apexcharts/apexcharts.min.js"></script>
+        <script src="template/assets/js/main.min.js"></script>
+        <script src="template/assets/js/pages/dashboard.js"></script>
+        <script src="template/assets/plugins/DataTables/datatables.min.js"></script>
+        <script src="template/assets/js/pages/datatables.js"></script>
+    </body>
+</html>
---
+++ template/sidebar.php
@@ -0,0 +1,16 @@
+
+            <div class="page-sidebar" style="height: auto;">
+                <ul class="list-unstyled accordion-menu">
+                  <li class="sidebar-title">
+                    Main
+                  </li>
+                  <li>
+                    <a href="index.php"><i data-feather="home"></i>Dashboard</a>
+                  </li>
+                  <li class="sidebar-title">
+                    Apps
+                  </li>
+                  <li>
+                    <a href="project.php"><i data-feather="inbox"></i>Management Project</a>
+                  </li>
+            </div>
